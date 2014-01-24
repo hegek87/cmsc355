ee621b69d7e31aef585b05122c3ae92f4808fb9b
hegek87 <hegelandka@vcu.edu>
2014-01-24 16:00:18 -0500

    Finished the coding, added comments, a second language file ('spanish.txt'). Added a few change ideas, and now need only write about how to use the program

--- project_1/Dictionary.java
+++ project_1/Dictionary.java
@@ -5,6 +5,17 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+/********************************************************
+*                  Dictionary
+* Translate an english word to some foreign language. The
+* language translated to by default is norwegian.
+*********************************************************
+* Input: String fileName, String englishWord -- fileName
+* is optional.
+* Output: Displays the foreign word to the user and will
+* write "English: englishWord, Foreign: TranslatedWord"
+* to the file output.txt
+********************************************************/
 public class Dictionary{
 	private String fileName;
 	private BufferedReader reader;
@@ -17,6 +28,11 @@ public class Dictionary{
 	// open files for reading and writing dictionary data
 	private void openDict(){
 		try{
+			/****************************************************
+			* Open reader and writer. The writer will write the
+			* results to output.txt, and the reader will read the
+			* (english, foreign) word dictionary file.
+			****************************************************/
 			FileReader fr = new FileReader(this.fileName);
 			FileWriter fw = new FileWriter("output.txt", true);
 			this.reader = new BufferedReader(fr);
@@ -29,6 +45,7 @@ public class Dictionary{
 	// clean up -- close the open readers
 	public void cleanUp(){
 		try{
+			// we are done reading and writing. Close our streams.
 			reader.close();
 			writer.close();
 		} catch(IOException ioe){
@@ -41,8 +58,10 @@ public class Dictionary{
 		String[] enFo;
 		String temp;
 		try{
+			// read from file until we reach eof (and get null)
 			while((temp = reader.readLine()) != null){
 				enFo = temp.split(", ");
+				// our word matches an (english, foreign) word pair
 				if(en.equals(enFo[0])){
 					return enFo[1];
 				}
@@ -56,19 +75,17 @@ public class Dictionary{
 	public BufferedReader getReader(){ return reader; }
 	public PrintWriter getWriter(){ return writer; }
 	
+	/*********************************************************
+	* Creates a dictionary and uses it to translate a supplied 
+	* english word to a foreign word. Writes the result to the
+	* user and output.txt
+	*********************************************************/
 	public static int processWord(String en, String fileName){
 		Dictionary d;
-		String temp;
-		if(fileName == null){
-			// no dictionary supplied
-			temp = "norwegian.txt";
-		}
-		else{
-			temp = fileName;
-		}
-		d = new Dictionary(temp);
+		d = new Dictionary(fileName);
 		d.openDict();
 		String foreign = d.translate(en);
+		// Determine if the (english, foreign) word pair was found
 		String found = (foreign != null) ? 
 						foreign : "Translation not found";
 		
@@ -81,41 +98,17 @@ public class Dictionary{
 		return 0;
 	}
 	
-	/*
-	*  args[0] == Dictionary
-	*  args[1] == English word
-	*  args[2] == fileName
-	*/
+	/***************************
+	*  args[0] == English word
+	*  args[1] == fileName
+	***************************/
 	public static void main(String[] args){
-	/*
-		System.out.println("IN MAIN");
-		for(int i = 0; i < args.length; ++i){
-			System.out.println(args[i]);
-		}*/
-		Dictionary d;
-		String temp = "norwegian.txt";
-		/*
-		if(args[1] != null){
-			System.out.println("IN IF");
-			temp = args[1];
-		}
-		else{
-			System.out.println("IN ELSE");
-			// no dictionary supplied
-			temp = "norwegian.txt";
-		}*/
-		d = new Dictionary(temp);
-		d.openDict();
-		String foreign = d.translate(args[0]);
-		String found = (foreign != null) ? 
-						foreign : "Translation not found";
-		// display to user
-		System.out.println(found);
-		d.getWriter().print("English: " + args[0]);
-		d.getWriter().println(", Foreign: " + found);
-		
-		d.cleanUp();
-		//processWord(args[0], ((args.length != 2) ? null : args[1]));
+		/****************************************************
+		* Check whether or not we should use the default file
+		* containing (english, norwegian) word pairs.
+		****************************************************/
+		String fileName = (args.length==2) ? args[1] : "norwegian.txt";
+		processWord(args[0], fileName);
 		
 	}
 		
--- project_1/Translate.java
+++ project_1/Translate.java
@@ -3,45 +3,92 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/********************************************************************
+*                          Translation Module
+* Repeatedly prompts the user for input, translating the words to a
+* foreign language.
+*********************************************************************
+* Input: String fileName
+* Output: Writes a file containing all english words entered by the
+* user and the translated words.
+*********************************************************************
+* Maintenance Log
+*********************************************************************
+* FIX001 01/24/14 Kenny Hegeland
+* Allowing an optional dictionary file to be supplied
+* FIX002 01/24/14 Kenny Hegeland
+* Restructured the Translate class to not contain all logic in main
+********************************************************************/
+
+/*******************************************************************
+*                            Translate Class
+* Gets user input and passes it to the Dictionary module to translate
+* it to a foreign word.
+*********************************************************************
+* Input: English word
+* Output: Foreign word
+********************************************************************/
 public class Translate{
 	public static final String cmd = "java -jar ";
 	public static final String dict = "Dictionary.jar ";
+	private Scanner in;
+	private Runtime runner;
 	
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		String fileName, en;
-		BufferedReader out;
-		InputStream is;
-		OutputStream os;
-		if(args.length != 1){
-			fileName = null;
-		}
-		else{
-			fileName = args[0];
-		}
+	public Translate(){
+		// Open scanner to read user input
+		this.in = new Scanner(System.in);
+		// Get a Runtime environment to call Dictionary
+		this.runner = Runtime.getRuntime();
+	}
+	
+	public void callDictionary(String en, String fileName){
+		String pCall = cmd + dict + en;
+		// Determine whether or not the user supplied a file
+		pCall += (fileName == null) ? "" : " " + fileName;
 		try{
-			Runtime runner = Runtime.getRuntime();
-			while(true){
-				System.out.print("> ");
-				en = in.nextLine();
-				System.out.println(en);
-				if(en.equals("q")){
-					break;
-				}
-				String pCall = cmd+dict+en+" "+fileName;
-				Process p = runner.exec(pCall);	
-				int retVal = p.waitFor();
-				
-				is = p.getInputStream();
-				int el;
-				while((el = is.read()) > 0){
-					System.out.print((char)el);
-				}
-			}
-		} catch(Exception e){
-			e.printStackTrace();
+			// Execute Dictionary.jar
+			Process p = this.runner.exec(pCall);
+			// Stream for reading output of the call to Dictionary.jar
+			InputStream is = p.getInputStream();
+			//wait for exec to finish
+			p.waitFor();
+			int el;
+			/**************************************************
+			* Read the output from the call to Dictionary.jar
+			**************************************************/
+			while((el = is.read()) > 0){ System.out.print((char)el); }
+		} catch(Exception e){ e.printStackTrace(); }
+	}
+	
+	public Scanner getIn(){ return this.in; }
+	
+	public static void run(String fileName){
+		Translate tran = new Translate();	
+		/**************************************************
+		* Continually get user input until user enters q.
+		**************************************************/
+		while(true){
+			System.out.print("> ");
+			// Read user input
+			String en = tran.getIn().nextLine();
+			// Has user entered q?
+			if(en.equals("q")){ return; }
+			tran.callDictionary(en, fileName);
 		}
+	}
+	
+	/********************
+	* args[0] == fileName
+	********************/
+	public static void main(String[] args){
+		// Scanner to get user input
+		String fName;
+		/*********************************************************
+		* Test if user has supplied an (english,foreign) word pair
+		* file.
+		*********************************************************/
+		if(args.length == 1){ fName = args[0]; }
+		else{ fName = null; }
+		run(fName);
 	}	
-}
-			
-			
+}			
